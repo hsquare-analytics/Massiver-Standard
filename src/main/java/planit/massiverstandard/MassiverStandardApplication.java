5b28d4b10270559a11f1472646a393644ceb4081
planit_mason <mason@masonui-MacBookAir.local>
2025-02-28 13:23:41 +0900

    init

---
+++ src/main/java/planit/massiverstandard/MassiverStandardApplication.java
@@ -0,0 +1,13 @@
+package planit.massiverstandard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MassiverStandardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MassiverStandardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/planit/massiverstandard/MassiverStandardApplicationTests.java
@@ -0,0 +1,13 @@
+package planit.massiverstandard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MassiverStandardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
